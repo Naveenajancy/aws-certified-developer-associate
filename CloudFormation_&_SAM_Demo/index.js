85dc6921a2ad6cae2261c5568b1e1212955f0436
nkb05 <kb.jancy90@gmail.com>
2021-07-15 14:06:57 -0400

    aws-developer-associate

--- CloudFormation_&_SAM_Demo/index.js
+++ CloudFormation_&_SAM_Demo/index.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello Cloud Gurus, This Lambda Function was deployed using SAM!')
+        body: JSON.stringify('Hello Cloud Developers, This Lambda Function was deployed using SAM!')
     };
     callback(null, response);
 };
